e449340a802556f93264b5dc92c32ca32c0c2437
Efinlearning <167295687+Efinlearning@users.noreply.github.com>
2024-12-29 17:42:16 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class BrokerCredentials(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    broker_username = db.Column(db.String(150), nullable=False)
+    broker_password = db.Column(db.String(150), nullable=False)
